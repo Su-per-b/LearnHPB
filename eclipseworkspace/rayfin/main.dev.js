23c4d7577d8375e7c0f595c57ce399d37e907263
rajdye <raj@pcdigi.com>
2011-09-17 06:58:36 -0700

    renamed project to rayfin

---
+++ eclipseworkspace/rayfin/main.dev.js
@@ -0,0 +1,15 @@
+
+goog.require ("lgb.BaseClass");
+goog.require ("lgb.controller.ControllerBase");
+goog.require ("lgb.controller.MainController");
+goog.require ("lgb.controller.LoaderController");		
+
+
+/*
+function mainxxxx() {
+  
+	 var mainController = new lgb.controller.MainController();
+	 mainController.init();
+    
+}
+*/
