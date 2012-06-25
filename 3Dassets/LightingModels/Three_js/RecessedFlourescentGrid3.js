8479c3d69f49a57387e417c34ae74126d5285647
rajdye <raj@pcdigi.com>
2012-06-25 13:50:36 -0700

    added some lighting assets

---
+++ 3Dassets/LightingModels/Three_js/RecessedFlourescentGrid3.js
@@ -0,0 +1,254 @@
+/* Converted from: RecessedFlourescentGrid3.blend
+ *
+ * File generated with Blender 2.58 Exporter
+ * https://github.com/mrdoob/three.js/tree/master/utils/exporters/blender/
+ *
+ * objects:    13
+ * geometries: 1
+ * materials:  4
+ * textures:   0
+ */
+
+var scene = {
+
+"type" : "scene",
+"urlBaseType" : "relativeToScene",
+
+
+"objects" :
+{
+    "B01" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 2.293000, 6.096000, 2.702000 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    },
+
+    "A12" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 35.820999, 2.285714, 2.702093 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    },
+
+    "A11" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 32.772999, 2.285714, 2.702093 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    },
+
+    "A10" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 29.725000, 2.285714, 2.702093 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    },
+
+    "A09" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 26.677000, 2.285714, 2.702093 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    },
+
+    "A08" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 23.629000, 2.285714, 2.702093 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    },
+
+    "A07" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 20.580999, 2.285714, 2.702093 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    },
+
+    "A06" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 17.533001, 2.285714, 2.702093 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    },
+
+    "A05" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 14.485000, 2.286000, 2.702093 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    },
+
+    "A04" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 11.437000, 2.285714, 2.702093 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    },
+
+    "A01" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 2.293000, 2.286000, 2.702000 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    },
+
+    "A03" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 8.389000, 2.285714, 2.702093 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    },
+
+    "A02" : {
+        "geometry"  : "geo_lightGeometry",
+        "groups"    : [  ],
+        "materials" : [ "Default", "Interior", "Interior Body", "black" ],
+        "position"  : [ 5.341000, 2.286000, 2.702000 ],
+        "rotation"  : [ 0.000000, -0.000000, 0.000000 ],
+        "quaternion": [ 1.000000, 0.000000, 0.000000, 0.000000 ],
+        "scale"     : [ 1.000000, 1.000000, 1.000000 ],
+        "visible"       : true,
+        "castsShadow"   : false,
+        "meshCollider"  : false,
+        "trigger"       : "None"
+    }
+},
+
+
+"geometries" :
+{
+    "geo_lightGeometry" : {
+        "type" : "ascii_mesh",
+        "url"  : "RecessedFlourescentGrid3.lightGeometry.js"
+    }
+},
+
+
+"materials" :
+{
+    "black" : {
+        "type": "MeshLambertMaterial",
+        "parameters": { "color": 0, "opacity": 1 }
+    },
+
+    "Default" : {
+        "type": "MeshLambertMaterial",
+        "parameters": { "color": 13158600, "opacity": 1 }
+    },
+
+    "Interior" : {
+        "type": "MeshLambertMaterial",
+        "parameters": { "color": 14342623, "opacity": 1 }
+    },
+
+    "Interior Body" : {
+        "type": "MeshLambertMaterial",
+        "parameters": { "color": 14023418, "opacity": 1 }
+    }
+},
+
+
+"transform" :
+{
+    "position"  : [ 0.000000, 0.000000, 0.000000 ],
+    "rotation"  : [ -1.570796, 0.000000, 0.000000 ],
+    "scale"     : [ 1.000000, 1.000000, 1.000000 ]
+},
+
+"defaults" :
+{
+    "bgcolor" : [ 0.000000, 0.000000, 0.000000 ],
+    "bgalpha" : 1.000000,
+    "camera"  : ""
+}
+
+}
+
+postMessage( scene );
+close();
