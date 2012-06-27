9bf49daf2b28be11c3e871ba85e8c700b2229f29
rajdye <raj@pcdigi.com>
2012-06-27 10:17:47 -0700

    lighting now works

---
+++ 3Dassets/LightingModels/Three_js/PendantLight.obj.js
@@ -0,0 +1,58 @@
+// Converted from: E:\LGBprivate\3Dassets\converter-obj\PendantLight.obj
+//  vertices: 1057
+//  faces: 860
+//  materials: 3
+//
+//  Generated with OBJ -> Three.js converter
+//  http://github.com/alteredq/three.js/blob/master/utils/exporters/convert_obj_three.py
+
+
+var model = {
+
+    "version" : 1,
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "center",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 131.372549,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "grey",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.705882, 0.693558, 0.702801],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 131.372549,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "white",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [1.0, 0.982541, 0.995635],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 131.372549,
+	"transparency" : 1.0
+	}],
+
+    "buffers": "PendantLight.obj.bin"
+
+};
+
+postMessage( model );
+close();
